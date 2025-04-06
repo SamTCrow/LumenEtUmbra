1e8136805092598efe5a3555b7dbf9bb51cf8697
SamTCrow <162122571+SamTCrow@users.noreply.github.com>
2025-04-06 18:43:00 +0000

    Deploying to gh-pages from @ SamTCrow/LumenEtUmbra@97da5cc331d8fc18cfc5a8a02c33ec0a5bc9e191 🚀

---
+++ _nuxt/CiUfJlGN.js
@@ -0,0 +1,1 @@
+import{d as t,q as s,c as a,t as n,Q as o,i as c}from"./Dm92snOv.js";const i=t({__name:"[slug]",setup(r){const e=s().path.slice(7);return(p,l)=>(c(),a("div",null,n(o(e)),1))}});export{i as default};
--- app/pages/Classes/[slug].vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup>
-	const path = useRoute().path.slice(9);
-</script>
-
-<template>
-	<div>{{ path }}</div>
-</template>
